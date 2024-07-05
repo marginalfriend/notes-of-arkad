f40f93bf2df00b26665326e0f9e776b0bc10ee79
marginalfriend <abdullahpsg@gmail.com>
2024-07-05 17:15:45 +0700

    [NEW] authSlice

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ export default function Login() {
     <main className="w-screen min-h-screen flex align-middle justify-center">
       <div className="border flex flex-col gap-4 h-min my-auto p-4 shadow">
         <div>
-          <h1 className="text-center">Login</h1>
+          <h1 className="text-center text-xl">Login</h1>
           <h2 className="text-xs text-center text-gray-600">Notes of Arkad</h2>
         </div>
         <div className="flex flex-col gap-1 text-sm">
